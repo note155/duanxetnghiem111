8173be420f95388936e8a03c79962cae383b6d6f
Windows 10 <Windows 10@21AK22-COM>
2024-05-02 08:54:59 +0700

    5/2/2024

---
+++ Shared/Model/Thongbao.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Model
+{
+    public class Thongbao
+    {
+        [Key]
+        public int Id { get; set; }
+        public string noidung { get; set; }
+        public string? link { get; set; }
+        public int? iduser { get; set; }
+        public int trangthai { get; set; }
+
+    }
+}
---
+++ Shared/ketnoi/IThongbao.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Shared.Model;
+
+namespace Shared.ketnoi
+{
+    public interface IThongbao
+    {
+        Task<Thongbao> addAsync(Thongbao tbao);
+        Task<List<Thongbao>> getall();
+        Task<List<Thongbao>> getbyid(int id);
+        Task<Thongbao> updateAsync(Thongbao tbao);
+    }
+}
